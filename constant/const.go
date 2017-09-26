5d076e38823455bbfce8322902ef345af3ad8ab8
rakin <rakin.ishmam@gmail.com>
2017-09-26 10:32:50 +0600

    init constant

---
+++ constant/const.go
@@ -0,0 +1,10 @@
+package constant
+
+// OpDivider divides operation key words
+const OpDivider string = ":"
+
+// ErrDivider devides error key words
+const ErrDivider string = "#"
+
+// UserRightDivider divides user and rights
+const UserRightDivider string = "&"
