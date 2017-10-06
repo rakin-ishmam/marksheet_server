30dcac7f2c36cd22a7c60ef9391e75bc4c6454ff
rakin <rakin.ishmam@gmail.com>
2017-10-06 20:53:54 +0600

    add user right spliter

--- constant/const.go
+++ constant/const.go
@@ -6,8 +6,5 @@ const OpSpliter string = ":"
 // ErrSpliter devides error key words
 const ErrSpliter string = "#"
 
-// UserRightSplit divides right between users
-const UserRightSplit string = "&"
-
-// RightSpliter divides user and rights
-const RightSpliter string = "*"
+// UserRightSpliter divides right between users
+const UserRightSpliter string = "*"
