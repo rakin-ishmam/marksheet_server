59639e313f112d286044405e4b32c8eea26189d2
rakin <rakin.ishmam@gmail.com>
2017-10-06 21:10:43 +0600

    write test for accessor add

--- access/access_test.go
+++ access/access_test.go
@@ -125,3 +125,54 @@ func TestAccessHas(t *testing.T) {
 		})
 	}
 }
+
+func TestAccessAdd(t *testing.T) {
+	acc, err := access.NewAccessor(
+		user.GlobalUser(),
+		fmt.Sprintf(
+			"%v",
+			access.NewUserRight(user.TestUser(), access.NewRights(access.Read, access.Write)),
+		),
+	)
+
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	tt := []struct {
+		name string
+		op   func()
+		res  testformat.ValueFunc
+		exp  testformat.ValueFunc
+	}{
+		{
+			"test1",
+			func() {
+				rt := acc.Add(user.TestUser() + "t")
+				rt.Add(access.Read)
+				rt.Add(access.Write)
+			},
+			func() interface{} {
+				return acc
+			},
+			func() interface{} {
+				return fmt.Sprintf(
+					"%v %v",
+					access.NewUserRight(user.TestUser(), access.NewRights(access.Read, access.Write)),
+					access.NewUserRight(user.TestUser()+"t", access.NewRights(access.Read, access.Write)),
+				)
+			},
+		},
+	}
+
+	for _, v := range tt {
+		t.Run(v.name, func(t *testing.T) {
+			v.op()
+			test := testformat.New(v.name, v.exp, v.res)
+			if err := test.Test(); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
