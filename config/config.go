2d382e08f5ec311ecd26e2f3670a8121f43a11b0
rakin <rakin.ishmam@gmail.com>
2017-10-22 15:13:54 +0600

    add constant for max and min limit of the dir name

--- config/config.go
+++ config/config.go
@@ -8,3 +8,9 @@ const MinUserName = 5
 
 // MaxShareUser represents miximum share limit
 const MaxShareUser = 100
+
+// MaxDirName provides maximum limist of the directory
+const MaxDirName = 255
+
+// MinDirName provides minimum limist of the directory
+const MinDirName = 1
